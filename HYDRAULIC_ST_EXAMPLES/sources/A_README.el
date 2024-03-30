7c44a75fa01e0882493c9851054cf46980dbf2ff
Ramón Pérez-Vara <ramon.vara@gmail.com>
2024-03-30 23:24:57 +0100

    Limitadores de los Comentarios Corregidos

--- HYDRAULIC_ST_EXAMPLES/sources/A_README.el
+++ HYDRAULIC_ST_EXAMPLES/sources/A_README.el
@@ -2,18 +2,18 @@
  LIBRARY: HYDRAULIC_ST_EXAMPLES
  FILE: A_README
  CREATION DATE: 24/03/2024
------------------------------------------------------------------------------------------*/
-/* This library contains the Hydraulic examples presented in chapter 6 of the thesis.
-/* More specifically, it contains the following examples:
-/*     
-/* Model Example01.eds es el Modelo de Prueba 1 de la Figura 6-1 construido haciendo uso
-/* de la Librería HYDRAULIC_ST que implementa el nuevo paradigma
-/*
-/* Model Example01_classic.eds es el Modelo de Prueba 1 de la Figura 6-1 construido haciendo 
-/* uso de la Librería HYDRAULIC_ST_CLASSIC que implementa la aproximación clásica
-/*
-/* Model Example02.eds es el Modelo de Prueba 2 de la Figura 6-3 construido haciendo uso
-/* de la Librería HYDRAULIC_ST que implementa el nuevo paradigma
-/*
-/* Model Example02_classic.eds es el Modelo de Prueba 1 de la Figura 6-3 construido haciendo 
-/* uso de la Librería HYDRAULIC_ST_CLASSIC que implementa la aproximación clásica
+-------------------------------------------------------------------------------------------
+ * This library contains the Hydraulic examples presented in chapter 6 of the thesis.
+ * More specifically, it contains the following examples:
+ *     
+ * Model Example01.eds es el Modelo de Prueba 1 de la Figura 6-1 construido haciendo uso
+ * de la Librería HYDRAULIC_ST que implementa el nuevo paradigma
+ *
+ * Model Example01_classic.eds es el Modelo de Prueba 1 de la Figura 6-1 construido haciendo 
+ * uso de la Librería HYDRAULIC_ST_CLASSIC que implementa la aproximación clásica
+ *
+ * Model Example02.eds es el Modelo de Prueba 2 de la Figura 6-3 construido haciendo uso
+ * de la Librería HYDRAULIC_ST que implementa el nuevo paradigma
+ *
+ * Model Example02_classic.eds es el Modelo de Prueba 1 de la Figura 6-3 construido haciendo 
+ * uso de la Librería HYDRAULIC_ST_CLASSIC que implementa la aproximación clásica*/
