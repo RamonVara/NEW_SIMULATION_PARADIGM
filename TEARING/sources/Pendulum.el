7f3019dc06f660b49753ccd588d2010461e7e469
Ramón Pérez-Vara <ramon.vara@gmail.com>
2024-04-09 21:26:07 +0200

    Updata

--- TEARING/sources/Pendulum.el
+++ TEARING/sources/Pendulum.el
@@ -1,27 +1,10 @@
-
-COMPONENT Pendulum
+COMPONENT PendulumTransient
 	DATA
-		REAL m = 1      UNITS "kg"   "Pendulum mass"
-		REAL L = 1      UNITS "m"    "Pendulum length"
-		REAL g = 9.8    UNITS "m2/s" "Gravity"
-		REAL Fx_o = 9.8 UNITS "N"    "Initial force"
+		REAL m = 1     
+		REAL L = 1      
+		REAL g = 9.8   
 	DECLS
-		REAL x, y
-		REAL T
-	CONTINUOUS
-		x**2 + y**2 = L**2
-		0 = Fx_o - T * x/L
-		0 = -m*g - T * y/L 
-END COMPONENT
-
-COMPONENT Pendulum1
-	DATA
-		REAL m = 1      UNITS "kg"   "Pendulum mass"
-		REAL L = 1      UNITS "m"    "Pendulum length"
-		REAL g = 9.8    UNITS "m2/s" "Gravity"
-	DECLS
-		REAL Fx
-		--REAL Fy
+		BOUND REAL Fx
 		REAL x, y
 		REAL T
 	CONTINUOUS
@@ -29,3 +12,19 @@ COMPONENT Pendulum1
 		m*x'' = Fx - T * x/L
 		m*y'' = -m*g + T * y/L 
 END COMPONENT
+
+COMPONENT PendulumSteady
+	DATA
+		REAL m = 1      
+		REAL L = 1      
+		REAL g = 9.8    
+	DECLS
+	   BOUND REAL Fx
+		REAL x, y
+		REAL T
+	CONTINUOUS
+		x**2 + y**2 = L**2
+		0 = Fx  - T * x/L
+		0 = -m*g - T * y/L 
+END COMPONENT
+
--- THERMAL_NEW/sources/version.el
+++
@@ -1,7 +0,0 @@
--- Define the version, date, author and comments associated to this library
-#LIBRARY_INFO
-	#VERSION = "1.0"
-	#DATE = "20/02/2024"
-	#AUTHOR = ""
-	#COMMENTS= "No comments"
-END #LIBRARY_INFO
